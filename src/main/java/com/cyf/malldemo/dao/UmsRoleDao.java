237ea6d4662779f029a6629a2b2ae9fb341a8ce2
kakk22 <1029839308@qq.com>
2020-05-20 11:11:32 +0800

    用户角色资源 查询 修改

---
+++ src/main/java/com/cyf/malldemo/dao/UmsRoleDao.java
@@ -0,0 +1,20 @@
+package com.cyf.malldemo.dao;
+
+import com.cyf.malldemo.mbg.model.UmsResource;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**自定义后台角色Dao
+ * @author by cyf
+ * @date 2020/5/20.
+ */
+public interface UmsRoleDao {
+
+    /**
+     * 根据角色id查询对应的资源
+     * @param RoleId
+     * @return
+     */
+    List<UmsResource> getResourceListByRoleId(@Param("roleId") Long RoleId);
+}
