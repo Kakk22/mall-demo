2757da2e0866966261103cf7855d417db97bf141
kakk22 <1029839308@qq.com>
2020-05-22 17:43:47 +0800

    CancelOrderSend

---
+++ src/main/java/com/cyf/malldemo/component/CancelOrderSend.java
@@ -0,0 +1,42 @@
+package com.cyf.malldemo.component;
+
+import com.cyf.malldemo.dto.QueueEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+/**取消订单发送者
+ * @author by cyf
+ * @date 2020/5/22.
+ */
+@Component
+public class CancelOrderSend {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CancelOrderSend.class);
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    // TODO: 2020/5/22 给延迟队列发送消息 
+    public void sendMessage(Long orderId,final Long delayTimes){
+        amqpTemplate.convertAndSend(QueueEnum.QUEUE_TTL_ORDER_CANCEL.getExchange(),
+                QueueEnum.QUEUE_TTL_ORDER_CANCEL.getRoutekey(),
+                orderId, new MessagePostProcessor() {
+                    @Override
+                    public Message postProcessMessage(Message message) throws AmqpException {
+                        //给消息设置延迟毫秒值
+                        message.getMessageProperties().setExpiration(String.valueOf(delayTimes));
+                        return null;
+                    }
+                });
+        LOGGER.info("send delay message orderId {}" ,orderId);
+    }
+
+
+}
